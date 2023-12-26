650e99dabb1a20d5abf800f89e6983bb2cddcb50
MXFire1906 <85999078+ManuelDogbatse@users.noreply.github.com>
2023-12-26 12:46:37 +0000

    Added script files to repo

--- contents/powershell_users_script/README.md
+++ contents/powershell_users_script/README.md
@@ -8,7 +8,11 @@
 
 ## Powershell Script
 
-Now that Active Directory has been setup, it is time to simulate a real-world example of Active Directory in use to manage a company's users and computers. Creating objects in Active Directory can be automated through the use of Powershell scripts, with the 
+Now that Active Directory has been setup, it is time to simulate a real-world example of Active Directory in use to manage a company's users and computers. Creating objects in Active Directory can be automated through the use of Powershell scripts, with the use of the Active Directory API.
+
+I have written two scripts: one in Python, and one in Powershell. The Python script is responsible for reformatting the names text file to ensure that no empty lines are added as AD users, and that there are no duplicate usernames from clashing first name initials and surname combinations. The Powershell script creates organisational units for each department in the departments text file, and then creates users using the formatted names text file and adds them to one of the three departments using a loop.
+
+To run these scripts, firstly
 
 ## Sections
 
---
+++ contents/powershell_users_script/files/format_names.py
@@ -0,0 +1,36 @@
+"""Removes duplicates from names.txt"""
+import os
+
+# Get absolute file path for text file
+path = os.getcwd()
+names_file = path + "\\names.txt"
+names_formatted_file = path + "\\names_formatted.txt"
+
+# Reads text file and saves lines to list
+with open(names_file, "r", encoding="utf-8") as file:
+    print("Reading file")
+    names_str = file.read()
+    # Removes empty lines in txt file
+    # Stores each name as a string in a list
+    names_list = [line for line in names_str.split("\n") if line.strip()]
+    usernames_list = [name[0].lower() + name.split()[1].lower() for name in names_list]
+
+print("Creating dictionary of full names and usernames")
+# Merge names and usernames together
+name_uname_list = zip(usernames_list, names_list)
+# Convert into a dictionary to remove duplicate usernames
+names_dict = dict(name_uname_list)
+
+print("Sorting dictionary in alphabetic order")
+names_dict = dict(sorted(names_dict.items()))
+
+print("Duplicates removed successfully")
+print(f"Original names list count: {len(names_list)}\nNew names list count: {len(names_dict)}")
+
+# Write new list of names to text file
+with open(names_formatted_file, "w", encoding="utf-8") as file:
+    print("Writing to file")
+    names_str = "\n".join(names_dict.values())
+    file.write(names_str)
+
+print("File write complete")
